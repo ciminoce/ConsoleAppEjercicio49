ddfbb9abeb63f8b5e84bcb12804b6e44a761a265
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-08-30 19:53:37 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio49/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace ConsoleAppEjercicio49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int contador = 1; contador <=5; contador++)
+            {
+                Console.Write("Ingrese un número cualquiera:");
+                var nroIngresado= int.Parse(Console.ReadLine());
+                //suma = suma + nroIngresado;
+                suma += nroIngresado;
+            }
+
+            Console.WriteLine($"La suma de los nros ingresados es {suma}");
+            Console.ReadLine();
+        }
+    }
+}
